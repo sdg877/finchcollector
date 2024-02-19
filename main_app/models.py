f6cbb07b14cc186641cdad6fe4c6442eecafc049
Sylvia Drake-Gill <sylviadrake-gill@sylvias-air.home>
2024-02-19 16:28:59 +0000

    CRUD functions

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date, timedelta
 
-
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
 # Create your models here.
 class Finch(models.Model):
@@ -10,4 +16,26 @@ class Finch(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
+    
+    def is_fed_today(self):
+        today = date.today()
+        return self.feeding_set.filter(date=today).exists()
+    
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+  )
 
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+    
+    class Meta:
+        ordering = ['-date']
